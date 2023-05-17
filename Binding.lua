01051aeba0d7c4c461e8d338ec82e1a9f9992332
dierythros <124314710+dierythros@users.noreply.github.com>
2023-05-17 18:47:28 -0400

    Update Binding.lua

--- Binding.lua
+++ Binding.lua
@@ -1,36 +1,29 @@
--- Dierythros 2023
-
 local CollectionService = game:GetService("CollectionService")
 
 local Binding = {}
 Binding.__index = Binding
 
-local function shouldBind(instance)
-	-- You can have your own restrictions on what objects should be bound here.
-	-- Return false if you want an object to not be bound.
-	return true
-end
-
-function Binding.new(object, tag)
+function Binding.new(object, tag, predicate)
 	local self = setmetatable({}, Binding)
 	self.objects = {}
 	self._added = CollectionService:GetInstanceAddedSignal(tag):Connect(function(instance)
-		if not shouldBind(instance) then return end
-		self.objects[instance] = object.new(instance)
+		if predicate(instance) then
+			self.objects[instance] = object.new(instance)
+		end
 	end)
 	self._removed = CollectionService:GetInstanceRemovedSignal(tag):Connect(function(instance)
 		if not self.objects[instance] then return end
-		self.objects[instance]:Destroy()
+		self.objects[instance]:destroy()
 		self.objects[instance] = nil
 	end)
 	return self
 end
 
-function Binding:Destroy()
+function Binding:destroy()
 	self._added:Disconnect()
 	self._removed:Disconnect()
 	for _, object in self.objects do
-		object:Destroy()
+		object:destroy()
 	end
 	setmetatable(self, nil)
 	table.clear(self)
